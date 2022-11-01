80a140e28a08702ea5acbe8d1e602c92ac3f4f66
Jakub Gause <gause.jakub@gmail.com>
2022-11-01 19:58:08 +0100

    package boilerplate init

---
+++ src/Signator.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sewega\Signator;
+
+class Signator
+{
+    // Build your next great package.
+}
---
+++ src/SignatorFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sewega\Signator;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Sewega\Signator\Skeleton\SkeletonClass
+ */
+class SignatorFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'signator';
+    }
+}
